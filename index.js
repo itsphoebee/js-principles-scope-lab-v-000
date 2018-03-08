3eab34878fccdedff8169fc644c0ec0336259bad
Phoebe Lau <phoebelau7@gmail.com>
2018-03-08 21:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution in this file!
 customerName = 'bob'
-function upperCaseCustomerName(customerName) {
-  customerName.toUpperCase()
+function upperCaseCustomerName() {
+  return customerName.toUpperCase()
 }

803243b546854d7a0e6b48476cdcad9c90b71a43
Phoebe Lau <phoebelau7@gmail.com>
2018-03-08 21:36:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your solution in this file!
 var customerName = 'bob';
-var leastFavoriteCustomer = 'bob';
+leastFavoriteCustomer = 'bob';
 
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
